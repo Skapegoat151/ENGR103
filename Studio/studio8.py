5fe1b1dfc4c923fb84a42ba32e26e89cb5f90924
Skapegoat151 <irwin.connor151@gmail.com>
2026-05-21 14:48:30 -0700

    Added a file

--- Studio/studio8.py
+++ Studio/studio8.py
@@ -1,3 +1,4 @@
+# Studio 6 but with 2D lists
 
 def userHours():
     check = True
@@ -46,66 +47,68 @@ def userClasses(day):
 
 
 def classTime(numClasses, day):
+    time = []
     hours = 0
     minutes = 0
     for i in range(1, numClasses+1):
         print("Enter the number of hours and the number of minutes for class", i, "on", day)
-        hours += userHours()
-        minutes += userMinutes()
-    
-    time = hours + (minutes/60)
+        hours = userHours()
+        minutes = userMinutes()
+        time.append(hours + (minutes/60))
 
     return time
 
 def main():
-    time1 = 0
-    time2 = 0
-    time3 = 0
-    time4 = 0
-    time5 = 0
+    
+    times = []
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
     for i in range(1, 6):
         if i == 1:
             classes = userClasses("Monday")
             if classes == 0:
-                continue
+                times.append(0)
             else:
-                time1 = classTime(classes, "Monday")
+                times.append(classTime(classes, "Monday"))
         
         elif i == 2:
             classes = userClasses("Tuesday")
             if classes == 0:
-                continue
+                times.append(0)
             else:
-                time2 = classTime(classes, "Tuesday")
+                times.append(classTime(classes, "Tuesday"))
 
         elif i == 3:
             classes = userClasses("Wednesday")
             if classes == 0:
-                continue
+                times.append(0)
             else:
-                time3 = classTime(classes, "Wenesday")
+                times.append(classTime(classes, "Wenesday"))
 
         elif i == 4:
             classes = userClasses("Thursday")
             if classes == 0:
-                continue
+                times.append(0)
             else:
-                time4 = classTime(classes, "Thursday")
+                times.append(classTime(classes, "Thursday"))
 
         elif i == 5:
             classes = userClasses("Friday")
             if classes == 0:
-                continue
+                times.append(0)
             else:
-                time5 = classTime(classes, "Friday")
+                times.append(classTime(classes, "Friday"))
 
-    print(f"You have {time1:.2f} hours of classes on Monday")
-    print(f"You have {time2:.2f} hours of classes on Tuesday")
-    print(f"You have {time3:.2f} hours of classes on Wednesday")
-    print(f"You have {time4:.2f} hours of classes on Thursday")
-    print(f"You have {time5:.2f} hours of classes on Friday")
+    for i in range(5):
+        if times[i] == 0:
+            print("\nYou do not have class on", days[i], end = " ")
+        else:
+            formatList = []
+            for j in range(len(times[i])):
+                formatList.append(f"{times[i][j]:.2f}")
+            print(f"\nYou have {sum(times[i]):.2f} hours of classes on {days[i]}, broken up into", end = " ")
+            for j in range(len(formatList)):
+                print(formatList[j], end = " ")
             
-
-
 
 main()
